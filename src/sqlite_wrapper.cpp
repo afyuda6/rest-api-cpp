f3519cd49243bda329b0224704663e8fda7f4f86
Yuda <yuda_p411@apps.ipb.ac.id>
2024-11-27 16:33:48 +0700

    27/11/2024

--- src/sqlite_wrapper.cpp
+++ src/sqlite_wrapper.cpp
@@ -4,7 +4,7 @@
 sqlite3* openDatabase(const std::string& dbFile) {
     sqlite3* db;
     if (sqlite3_open(dbFile.c_str(), &db)) {
-        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+        std::cerr << "SQL error (open database): " << sqlite3_errmsg(db) << std::endl;
         return nullptr;
     }
     return db;
