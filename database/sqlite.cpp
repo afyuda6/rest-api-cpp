027825f9ddb66b769fd4d8607b02e9f2f2bdb58a
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-16 00:45:41 +0700

    16/12/2024

--- database/sqlite.cpp
+++ database/sqlite.cpp
@@ -2,22 +2,16 @@
 #include <sqlite3.h>
 
 void initializeDatabase() {
-    sqlite3* db;
+    sqlite3 *db;
     sqlite3_open("rest_api_cpp.db", &db);
-    if (db) {
-        char *errMessage = nullptr;
-        const char *dropUsersTableSQL = "DROP TABLE IF EXISTS users;";
-        if (sqlite3_exec(db, dropUsersTableSQL, nullptr, nullptr, &errMessage) != SQLITE_OK) {
-            std::cerr << "SQL error (drop users table): " << errMessage << std::endl;
-            sqlite3_free(errMessage);
-        }
-        const char *createUsersTableSQL = "CREATE TABLE IF NOT EXISTS users ("
-                "id INTEGER PRIMARY KEY, "
-                "name TEXT NOT NULL);";
-        if (sqlite3_exec(db, createUsersTableSQL, nullptr, nullptr, &errMessage) != SQLITE_OK) {
-            std::cerr << "SQL error (create users table): " << errMessage << std::endl;
-            sqlite3_free(errMessage);
-        }
-        sqlite3_close(db);
-    }
+    char *errMessage = nullptr;
+    const char *dropUsersTableSQL = "DROP TABLE IF EXISTS users;";
+    sqlite3_exec(db, dropUsersTableSQL, nullptr, nullptr, &errMessage);
+    sqlite3_free(errMessage);
+    const char *createUsersTableSQL = "CREATE TABLE IF NOT EXISTS users ("
+        "id INTEGER PRIMARY KEY, "
+        "name TEXT NOT NULL);";
+    sqlite3_exec(db, createUsersTableSQL, nullptr, nullptr, &errMessage);
+    sqlite3_free(errMessage);
+    sqlite3_close(db);
 }
