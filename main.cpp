940fde6691f53f8fd35c6a656cd22d414891eca5
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-15 23:18:19 +0700

    15/12/2024

--- main.cpp
+++ main.cpp
@@ -16,30 +16,23 @@ void userHandler(const std::string &request, const std::unordered_map<std::strin
 
 int main() {
     initializeDatabase();
-
     WSADATA wsaData;
     SOCKET clientSocket;
     sockaddr_in server{}, client{};
     int c;
     char buffer[1024];
-
     WSAStartup(MAKEWORD(2, 2), &wsaData);
-
     const SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == INVALID_SOCKET) {
         return 0;
     }
-
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(6000);
-
     if (bind(serverSocket, reinterpret_cast<sockaddr *>(&server), sizeof(server)) == SOCKET_ERROR) {
         return 0;
     }
-
     listen(serverSocket, 3);
-
     c = sizeof(sockaddr_in);
     while ((clientSocket = accept(serverSocket, reinterpret_cast<sockaddr *>(&client), &c)) != INVALID_SOCKET) {
         if (int read_size = recv(clientSocket, buffer, sizeof(buffer), 0); read_size > 0) {
@@ -48,10 +41,8 @@ int main() {
             std::string request(buffer);
             userHandler(request, params, clientSocket);
         }
-
         closesocket(clientSocket);
     }
-
     closesocket(serverSocket);
     WSACleanup();
     return 0;
